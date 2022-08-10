ab0c49bfbd064c4c5818d50081fbf51a18954347
NarendraRaj19 <ramasamykanchanama.n@northeastern.edu>
2022-08-09 21:22:36 -0400

    Working Code

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+//To Parse Request JSON
 var bodyParser = require('body-parser')
+//To Perform a lookup to validate a given URL
 var dns = require('dns');
+//To Generate Short URL 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -11,11 +14,16 @@ app.use(bodyParser.json());
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+const baseUrl = "http://localhost:3000/api/shorturl";
+
+const collectionOfURLs = new Set();
+const urlMappings = new Map();
+const urlMappings2 = new Map();
+
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-let responseObject = {};
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
@@ -27,33 +35,59 @@ app.get('/api/hello', function(req, res) {
 });
 
 app.post('/api/shorturl', function(req, res) {
-
-  //Regex to handle scenarios where https in present in the url as DNS lookup can't handle it
-  const REPLACE_REGEX = /^https?:\/\//i;
-
   const url = req.body.url;
 
-  //remove https part if present from the url
-  const finalUrl = url.replace(REPLACE_REGEX, '');
-
-  console.log("Final URl", finalUrl);
-
-  var w3 = dns.lookup(finalUrl, function (err, addresses, family) {
-    
-    if(err){
-      console.log("Invalid URL !!");
-      //responseObject["error"] = 'invalid url';
+  if(!url.includes('https://')){
+      console.log("Doesn't include Https Part")
       res.json({"error":"invalid url"});
-    } else {
-      console.log("valid URL !!");
-      //responseObject["original_url"] = url;
-      res.json({"original_url":url});
-    }
-    console.log(addresses);
-    console.log(family);
+  } else {
 
-    //res.send("empty");
+      //Regex to handle scenarios where https in present in the url as DNS lookup can't handle it
+      const REPLACE_REGEX = /^https?:\/\//i;
+      //remove https part if present from the url as dns cant handle https part while lookup
+      const finalUrl = url.replace(REPLACE_REGEX, '');
+
+      console.log("Final URl", finalUrl);
+
+      var w3 = dns.lookup(finalUrl, function (err, addresses, family) {
+        
+        if(err){
+          console.log("Invalid URL !!");
+          //responseObject["error"] = 'invalid url';
+          res.json({"error":"invalid url"});
+        } else {
+          console.log("valid URL !!");
+          //responseObject["original_url"] = url;
+
+          if(collectionOfURLs.has(url)){
+            var mappedShortURLValue = urlMappings.get(url);
+            res.json({"original_url":url,"short_url":mappedShortURLValue});
+          } else {
+            var shortURL = Math.random().toString(32).substring(2, 5);
+            console.log("Generated Short URL: ", shortURL)
+            //console.log("Generated Total Short URL: ", baseUrl +'/'+ shortURL)
+            collectionOfURLs.add(url);
+            urlMappings.set(url, shortURL);
+            urlMappings2.set(shortURL, url);
+            res.json({"original_url":url,"short_url":shortURL});
+          }
+          
+        }
   });
+  }
+});
+
+app.get("/api/shorturl/:shortURL", function(req,res){
+    var shortURL = req.params.shortURL;
+    //Retrieve Original from Map using ShortURL value passed
+    var originalVal = urlMappings2.get(shortURL);
+    if(originalVal){
+      res.redirect(originalVal);
+    } else  {
+      console.log("Entered Wrong Short URL")
+      res.json({"error":"Wrong format"});
+    }
+    
 });
 
 
