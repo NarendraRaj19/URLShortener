f6e61c496fc41579c58d8cb61d443332e2dd3f34
NarendraRaj19 <ramasamykanchanama.n@northeastern.edu>
2022-08-10 01:03:55 -0400

    Final Push

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ let uri = 'mongodb+srv://narenrj:AtlasPassword@freecodecamp-backendapi.kuafusj.m
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 console.log("State of Mongoose: ",mongoose.connection.readyState);
 
-//Define Schema
-let urlSchema = new mongoose.Schema({ original : 'string' });
-
-let Url = mongoose.model('Url', urlSchema)
+let Url = mongoose.model('Url', {
+  original: { type: String },
+  shortURL: { type: String }
+})
 
 app.use(cors());
 
@@ -48,26 +48,37 @@ app.post('/api/shorturl', async function(req, res) {
     if(!address){
       res.json({"error":"Wrong format"});
     } else {
-      const url = new Url({ original: originalUrl})
-      url.save((err,data) => {
-        res.json({
-          original_url: data.original,
-          short_url: data.id
-        })
-      })
+      Url.findOne({original : originalUrl}, (err, docs) => {
+        if(!docs){
+            let generateShortUrl = Math.random().toString(32).substring(2, 5);
+            const url = new Url({ original: originalUrl, shortURL:generateShortUrl })
+            url.save((err,data) => {
+              res.json({
+                original_url: data.original,
+                short_url: data.shortURL
+              })
+            })
+        } else {
+          res.json({
+            original_url: docs.original,
+            short_url: docs.shortURL
+          })
+        }
+      });
+
+      
     }
   });
 })
-// Math.random().toString(32).substring(2, 5)
 
 app.get('/api/shorturl/:input', function(req, res) {
   let inputUrl = req.params.input;
-
-  Url.findById(inputUrl, (err, data) => {
-    if(!data){
+  //console.log("Provide Short URL: ", inputUrl);
+  Url.findOne({shortURL : inputUrl}, (err, docs) => {
+    if(!docs){
       res.json({"error":"Wrong format"});
     } else {
-      res.redirect(data.original)
+      res.redirect(docs.original)
     }
   })
   
